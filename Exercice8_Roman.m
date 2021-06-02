42cf874f208d79f954a442badba4bbcd1c379390
Roman Stemplowski <roman.galaxiechips@gmail.com>
2021-06-02 16:40:11 +0200

    Ajouté detection de particule

--- Exercice8_Roman.m
+++ Exercice8_Roman.m
@@ -36,7 +36,7 @@ pi = 3.14159265358979323846264338327950288419716939937510582097494459230;
 %% Etude de convergence ================================================= %
 input = 'configuration2i.in';
 doSimulation = 0;           % 1 = TRUE    0 = FALSE
-loadData = 0;               % Pour grosse simulation déjà chargée dans MATLAB
+loadData = 1;               % Pour grosse simulation déjà chargée dans MATLAB
 %=========================================================================%
 tfin = 2000;
 Nsimul = 30;
@@ -46,6 +46,8 @@ nomFigure{1} = '8_2i_convergence';
 
 %=========================================================================%
 %=========================================================================%
+
+
 
 dt = tfin./N;
 if loadData == 1
@@ -153,12 +155,133 @@ plot(t, DeltaxDeltap);
 hold on; grid on;
 yline(0.5, 'k--','$\frac{\hbar}{2}$', 'fontsize', 20,'linewidth', 2,'LabelHorizontalAlignment', 'left','LabelVerticalAlignment','bottom', 'Interpreter', 'latex');
 xlabel('$t$ [s]');
-ylabel('$\langle \Delta x \rangle \cdot \langle \Delta p \rangle$');
+ylabel('$\langle \Delta x \rangle \cdot \langle \Delta p \rangle$  [a.u]');
 ylim([0.4 0.9]);
+
+%% Détection de particules (indépendant) ================================ %
+input = 'configuration2iv.in';
+doSimulation = 0;           % 1 = TRUE    0 = FALSE
+loadData = 0;               % Pour grosse simulation déjà chargée dans MATLAB
+%=========================================================================%
+t_detec = [-1.0, 800.0];
+limdet = [100, 180];
+dt = 1;
+%=========================================================================%
+nomFigure{5} = '8_2vi_x';
+nomFigure{6} = '8_2vi_p';
+nomFigure{7} = '8_2vi_H';
+nomFigure{8} = '8_2vi_Dx';
+nomFigure{9} = '8_2vi_Dp';
+
+nomFigure{12} = 'Potentiel';
+nomFigure{11} = 'Psi2_without';
+nomFigure{10} = 'Psi2_with';
+%=========================================================================%
+%=========================================================================%
+
+detection = {'without', 'with'};
+if loadData == 1
+    for i = 1:size(t_detec')
+        nomfile{i,1} = [detection{i}, '_detection_pot.out'];
+        nomfile{i,2} = [detection{i}, '_detection_sqwave.out'];
+        nomfile{i,3} = [detection{i}, '_detection_obs.out'];
+        if doSimulation == 1
+            cmd = sprintf('%s%s %s output_potential=%s output_squared_wave=%s output_observables=%s t_detect=%.15g xda=%.15g xdb=%.15g dt=%.15g', ...
+                repertoire, executable, input, nomfile{i,1}, nomfile{i,2}, nomfile{i,3}, t_detec(i), limdet(1), limdet(2), dt);     % Pour rajouter parametre numérique : %s=%.15g
+            disp(cmd)
+            system(cmd);
+        end
+    end
+    % Charger potentiel
+    data = load(nomfile{1,1});
+    V = data(:,2);
+    % Charger psi2
+    [t, X, psi2_wout] = charger_psi2(nomfile{1,2});
+    [t, X, psi2_with] = charger_psi2(nomfile{2,2});
+    % Charger observables
+    [x_wout, p_wout, H_wout, Dx_wout, Dp_wout] = charger_obs(nomfile{1,3});
+    [x_with, p_with, H_with, Dx_with, Dp_with] = charger_obs(nomfile{2,3});
+end
+
+% PLot potentiel ======================
+fig{12} = figure('Name',nomFigure{12},'NumberTitle','off');
+plot(X, V);
+hold on; grid on;
+xlabel('$x$ [a.u]');
+ylabel('$V$ [a.u]');
+
+% Plot without ========================
+fig{11} = figure('Name',nomFigure{11},'NumberTitle','off');
+[x,T] = meshgrid(X,t);
+pcolor(x,T,psi2_wout);
+c = colorbar('TickLabelInterpreter', 'latex');
+shading interp
+colormap jet
+xlabel('$x$ [a.u]');
+ylabel('$t$ [s]');
+ylabel(c,'$|\psi|^2$','Interpreter', 'latex');
+
+% Plot with ===========================
+fig{10} = figure('Name',nomFigure{10},'NumberTitle','off');
+[x,T] = meshgrid(X,t);
+pcolor(x,T,psi2_with);
+c = colorbar('TickLabelInterpreter', 'latex');
+shading interp
+colormap jet
+xlabel('$x$ [a.u]');
+ylabel('$t$ [s]');
+ylabel(c,'$|\psi|^2$','Interpreter', 'latex');
+
+fs = 12;
+% Plot observable =====================
+fig{5} = figure('Name',nomFigure{5},'NumberTitle','off');
+plot(t, x_wout, 'Displayname', 'Without detection');
+hold on; grid on;
+plot(t, x_with, 'Displayname', 'With detection');
+xline(t_detec(2),'k--', 'Detection','linewidth', 1.5, 'FontSize', fs,'LabelHorizontalAlignment', 'left','LabelVerticalAlignment', 'bottom', 'HandleVisibility','off'); % 'LabelOrientation', 'horizontal', 'LabelVerticalAlignment', 'bottom');
+xlabel('$t$ [s]');
+ylabel('$\langle x \rangle$ [a.u]');
+legend('location', 'best');
+
+fig{6} = figure('Name',nomFigure{6},'NumberTitle','off');
+plot(t, p_wout, 'Displayname', 'Without detection');
+hold on; grid on;
+plot(t, p_with, 'Displayname', 'With detection');
+xline(t_detec(2),'k--', 'Detection','linewidth', 1.5, 'FontSize', fs,'LabelHorizontalAlignment', 'left','LabelVerticalAlignment', 'bottom', 'HandleVisibility','off'); % 'LabelOrientation', 'horizontal', 'LabelVerticalAlignment', 'bottom');
+xlabel('$t$ [s]');
+ylabel('$\langle p \rangle$ [a.u]');
+legend('location', 'best');
+
+fig{7} = figure('Name',nomFigure{7},'NumberTitle','off');
+plot(t, H_wout, 'Displayname', 'Without detection');
+hold on; grid on;
+plot(t, H_with, 'Displayname', 'With detection');
+xline(t_detec(2),'k--', 'Detection','linewidth', 1.5, 'FontSize', fs,'LabelHorizontalAlignment', 'left','LabelVerticalAlignment', 'bottom', 'HandleVisibility','off'); % 'LabelOrientation', 'horizontal', 'LabelVerticalAlignment', 'bottom');
+xlabel('$t$ [s]');
+ylabel('$\langle H \rangle$ [a.u]');
+legend('location', 'best');
+
+fig{8} = figure('Name',nomFigure{8},'NumberTitle','off');
+plot(t, Dx_wout, 'Displayname', 'Without detection');
+hold on; grid on;
+plot(t, Dx_with, 'Displayname', 'With detection');
+xline(t_detec(2),'k--', 'Detection','linewidth', 1.5, 'FontSize', fs,'LabelHorizontalAlignment', 'left','HandleVisibility','off'); % 'LabelOrientation', 'horizontal', 'LabelVerticalAlignment', 'bottom');
+xlabel('$t$ [s]');
+ylabel('$\langle \Delta x \rangle$ [a.u]');
+legend('location', 'best');
+
+fig{9} = figure('Name',nomFigure{9},'NumberTitle','off');
+plot(t, Dp_wout, 'Displayname', 'Without detection');
+hold on; grid on;
+plot(t, Dp_with, 'Displayname', 'With detection');
+xline(t_detec(2),'k--', 'Detection','linewidth', 1.5, 'FontSize', fs,'LabelHorizontalAlignment', 'left','LabelVerticalAlignment', 'bottom', 'HandleVisibility','off'); % 'LabelOrientation', 'horizontal', 'LabelVerticalAlignment', 'bottom');
+xlabel('$t$ [s]');
+ylabel('$\langle \Delta p \rangle$ [a.u]');
+legend('location', 'best');
 
 %% Save figures ========================================================= %
 saveFigure = 0;         %1 = TRUE, 0 = FALSE
-sauver = 0;%[1 2 3 4 5];
+sauver = 1:12;
 
 if saveFigure == true
     for i=sauver
@@ -169,5 +292,18 @@ end
 
 %% Fonctions utilisées
 
+function [t, X, psi2] = charger_psi2(nomfile)
+    data = load(nomfile);
+    t = data(2:end,1);
+    X = data(1,2:end);
+    psi2 = data(2:end, 2:end);
+end
 
-
+function [x, p, H, Dx, Dp] = charger_obs(nomfile)
+    data = load(nomfile);
+    x = data(:,6);
+    p = data(:,8);
+    H = data(:,10);
+    Dx = data(:,11);
+    Dp = data(:,12);
+end
